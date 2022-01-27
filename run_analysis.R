41233cb0c7a65b07f01f38f81de287463db42ca3
practicingR <97630782+practicingR@users.noreply.github.com>
2022-01-27 15:56:32 -0500

    Read each table and merge datasets
    Read tables into R and used descriptive names for table names and column names. Merged all the tables together to all be in a table named "mergeddata"

--- run_analysis.R
+++ run_analysis.R
@@ -17,3 +17,30 @@
 
     library(dplyr)
 
+## Read each file and merge datasets
+
+   ### Use `read.table()` to import datasets into R and use descriptive names to name each table and columns within them
+
+    activitylabels <- read.table("~/R/R Course/Course3-ProgrammingAssignment/Course3PeerAssignment/UCI HAR Dataset/activity_labels.txt", quote="\"", comment.char="", col.names = c("idnumber", "activity"))
+
+    features <- read.table("~/R/R Course/Course3-ProgrammingAssignment/Course3PeerAssignment/UCI HAR Dataset/features.txt", quote="\"", comment.char="", col.names = c("idnumber", "feature"))
+
+    trainingsubject <- read.table("~/R/R Course/Course3-ProgrammingAssignment/Course3PeerAssignment/UCI HAR Dataset/train/subject_train.txt", quote="\"", comment.char="", col.names = "subject")
+
+    trainingset <- read.table("~/R/R Course/Course3-ProgrammingAssignment/Course3PeerAssignment/UCI HAR Dataset/train/X_train.txt", quote="\"", comment.char="", col.names = c(features$feature))
+
+    traininglabels <- read.table("~/R/R Course/Course3-ProgrammingAssignment/Course3PeerAssignment/UCI HAR Dataset/train/Y_train.txt", quote="\"", comment.char="", col.names = "idnumber")
+
+    testsubject <- read.table("~/R/R Course/Course3-ProgrammingAssignment/Course3PeerAssignment/UCI HAR Dataset/test/subject_test.txt", quote="\"", comment.char="", col.names = "subject")
+
+    testset <- read.table("~/R/R Course/Course3-ProgrammingAssignment/Course3PeerAssignment/UCI HAR Dataset/test/X_test.txt", quote="\"", comment.char="", col.names = c(features$feature))
+
+    testlabels <- read.table("~/R/R Course/Course3-ProgrammingAssignment/Course3PeerAssignment/UCI HAR Dataset/test/Y_test.txt", quote="\"", comment.char="", col.names = "idnumber")
+
+   ### Merge all the data into one set named `mergeddata`
+
+    subject <- rbind(trainingsubject, testsubject)
+    dataset <- rbind(trainingset, testset)
+    labels <- rbind(traininglabels, testlabels)
+
+    mergeddata <- cbind(subject, labels, dataset)
